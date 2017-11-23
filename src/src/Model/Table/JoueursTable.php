8b3a3302e91f3871cc450e33d79c2b93090dab4b
Raitoning <Rayman54@hotmail.fr>
2017-11-23 09:48:30 +0100

    Ajout JoueursTable.

---
+++ src/src/Model/Table/JoueursTable.php
@@ -0,0 +1,14 @@
+<?php
+//	src/Model/Table/JoueursTable.php
+namespace App\Model\Table;
+
+use Cake\ORM\Table;
+
+class JoueursTable extends Table
+{
+	public function initialize(array $config)
+    {
+		
+    }
+}
+?>
