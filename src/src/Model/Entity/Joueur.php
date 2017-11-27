dbb05c49270bda01b3aec773278dd920c487613f
pelte3u <pelte.gaetan9@etu.univ-lorraine.fr>
2017-11-27 18:01:54 +0100

    Ajout du hashage du mot de passe

--- src/src/Model/Entity/Joueur.php
+++ src/src/Model/Entity/Joueur.php
@@ -1,13 +1,14 @@
 <?php
 // src/Model/Entity/Joueur.php
 namespace App\Model\Entity;
+use Cake\Auth\DefaultPasswordHasher; // Add this line
 
 use Cake\ORM\Entity;
 
 class Joueur extends Entity
 {
 	protected $_accessible = [
-	
+
 		'*' => false,
 		'id' => false,
 		'pseudo' => false,
@@ -16,5 +17,15 @@ class Joueur extends Entity
 		'manche' => false,
 		'cartes' => false,
 	];
+
+	// Add this method
+    protected function _setPassword($value)
+    {
+        if (strlen($value)) {
+            $hasher = new DefaultPasswordHasher();
+
+            return $hasher->hash($value);
+        }
+    }
 }
 ?>
